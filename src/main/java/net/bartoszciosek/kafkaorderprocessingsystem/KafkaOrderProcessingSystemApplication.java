23a19df824dabbe22e66e387f34c70105fa09c51
bartoszciosek <ciosek000@gmail.com>
2026-07-23 12:45:54 +0200

    initial

---
+++ src/main/java/net/bartoszciosek/kafkaorderprocessingsystem/KafkaOrderProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package net.bartoszciosek.kafkaorderprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOrderProcessingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bartoszciosek/kafkaorderprocessingsystem/KafkaOrderProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bartoszciosek.kafkaorderprocessingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderProcessingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
